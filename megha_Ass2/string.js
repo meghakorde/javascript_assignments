2e0d4f6bfe03355e6784e1d9b0a9341fcc1873e3
meghakorde <megha.perficient@gmail.com>
2022-01-10 19:01:23 +0530

    updated ass 2

--- megha_Ass2/string.js
+++ megha_Ass2/string.js
@@ -1,14 +1,15 @@
+
 function form_output() {
     var email = document.getElementById('Email_id').value;
     var first_name = document.getElementById('first_name').value;
     var last_name = document.getElementById('last_name').value;
+    var age = document.getElementById('age').value;
+    age = parseInt(age);
 
     document.getElementById('emailid').innerHTML = email;
     document.getElementById('firstname').innerHTML= first_name.charAt(0).toUpperCase() + first_name.slice(1);
     document.getElementById('lastname').innerHTML= last_name.charAt(0).toUpperCase() + last_name.slice(1);
-}
-function age_output() {
-    var age = document.getElementById('age').value;
+    var age_result= document.getElementById("age");
     if (age == '') {
         alert("Please enter the age");
     }
@@ -16,6 +17,18 @@ function age_output() {
         alert("You are genuis");
     }
     else {
-        alert("You are kid");   
+        alert( "You are kid");   
     }
 }
+// function age_output() {
+//     var age = document.getElementById('age').value;
+//     if (age == '') {
+//         alert("Please enter the age");
+//     }
+//     else if (age > 10) {
+//         alert("You are genuis");
+//     }
+//     else {
+//         alert("You are kid");   
+//     }
+// }
