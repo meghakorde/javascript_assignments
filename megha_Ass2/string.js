e4cd574f2aaf0768a60508d245cf399e4a305982
meghakorde <megha.perficient@gmail.com>
2022-01-19 15:50:41 +0530

    updated

--- megha_Ass2/string.js
+++ megha_Ass2/string.js
@@ -10,23 +10,14 @@ result.addEventListener("submit",function(e){
     document.getElementById('emailid').innerHTML = email;
     document.getElementById('firstname').innerHTML= first_name.charAt(0).toUpperCase() + first_name.slice(1);
     document.getElementById('lastname').innerHTML= last_name.charAt(0).toUpperCase() + last_name.slice(1);
-   //  var age_result = document.getElementById('age');
-   //  if (age > 10) {
-   //    age_result.innerHTML="You are genuis";
-   //  }
-   //  else if (age < 10) {
-   //     age_result.innerHTML="you are kid";
-   //  }
-   //  else {
-   //     age_result.innerHTML=" Please enter the age ";   
-   //  } 
     if (age > 10) {
-      alert("You are genuis");
+     document.getElementById('age_result').innerHTML="You are genuis";
     }
     else if (age < 10) {
-       alert("you are kid");
+      document.getElementById('age_result').innerHTML="you are kid";
     }
     else {
-       alert(" Please enter the age ");   
+      document.getElementById('age_result').innerHTML=" Please enter the age ";   
     } 
+   
 }) 
